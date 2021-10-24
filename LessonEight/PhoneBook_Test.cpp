0a04937600d2780b7e5470818ecfeeab972093f9
Shini <kerlaeda3@yandex.ru>
2021-10-24 19:53:19 +0300

    LessonEight

--- LessonEight/PhoneBook_Test.cpp
+++
@@ -1,8 +0,0 @@
-#include "PhoneBook_Test.h"
-void PhoneBook_Test::SetUp(){
-    phoneBookTest = new PhoneBook::PhoneBook("Kozlovskii Artem Daniilovich", "81 8281 7420182 1");
-}
-
-void PhoneBook_Test::TearDown(){
-    delete phoneBookTest;
-}
