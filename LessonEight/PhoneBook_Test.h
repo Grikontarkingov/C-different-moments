708f2cabae2b902acdc33fe466359cc5cc7a588c
Shini <kerlaeda3@yandex.ru>
2021-10-24 19:54:58 +0300

    LessonEight

---
+++ LessonEight/PhoneBook_Test.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef _PHONEBOOK_TEST_H
+#define _PHONEBOOK_TEST_H
+
+#include "PhoneBook.h"
+#include "gtest/gtest.h"
+class PhoneBook_Test : public testing::Test{
+protected:
+    void SetUp() override{
+        phoneBookTest = new PhoneBook::PhoneBook("Kozlovskii Artem Daniilovich", "81 8281 7420182 1");
+    }
+
+    void TearDown() override{
+        delete phoneBookTest;
+    }
+
+    PhoneBook::PhoneBook *phoneBookTest;
+};
+
+#endif //_PHONEBOOK_TEST_H
